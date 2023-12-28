359e501273462d563a23f4434913e2b2fd5698f9
evrlong <erilok51657@stud.noroff.no>
2023-12-28 13:47:03 +0100

    Div html css

--- js/headphones.js
+++ js/headphones.js
@@ -1,40 +1,68 @@
+var powerButton = document.querySelector(".modePower")
+var ambientButton = document.querySelector(".modeAmbient")
+var fullButton = document.querySelector(".modeFull")
+
+var cancelStatus = document.querySelector(".modeStatus")
+
+function noiseCancelPwr() {
+    if (powerButton.innerHTML === "OFF") {
+        powerButton.innerHTML = "ON";
+        powerButton.style.background = "Green";
+        cancelStatus.innerHTML ="Noise Cancelling mode is ON";
+
+    }
+    else  {
+        powerButton.innerHTML = "OFF"
+        powerButton.style.background = "Red";
+        cancelStatus.innerHTML ="Noise Cancelling mode is OFF";
+    }
+}
+powerButton.onclick = noiseCancelPwr;
+
+
+
+
+
+
 const headphones = {
     brand: { 
-        Name:"Sony"
-        hq: "Japan"        
-}
+        Name:"Sony",
+        hq: "Japan",        
+},
 
     model: {
-        Name: "wh 1000mx3"
-        yearProd: 2018
-    }
+        Name: "wh 1000mx3",
+        yearProd: 2018,
+    },
     usbC: true,
-    wireless: true;
+    wireless: true,
     isOn: false,
     batterylevel: 50,
     volume: 75,
     noiseCancelMode: [{
-        name: "on"
+        name: "on",
         noiseCancel: 50,
     },
     {
-        name: "off"
+        name: "off",
         noiseCancel: 0,
     },
     {
-        name: "ambient"
+        name: "ambient",
         noiseCancel: 30,
     },
     {
-        name: "full"
+        name: "full",
         noiseCancel: 100,
-    }]
+    }],
 
     
 powerOn: function(){
-    headphones.on = !motorbike.on
+    headphones.on = !headphones.on
 }
 }
+
+
 
     
   
